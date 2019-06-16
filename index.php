d010d5fec15c7ff55e05a2861ea5da76eb1565c5
TJ Harrop <tjharrop@gmail.com>
2019-06-16 21:05:43 +1000

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php include_once("index.html"); ?>
-
-<!-- This PHP file is a workaround for heroku. You cannot push an HTML, CSS and JS site to heroku.
-Adding a PHP file which effectively does nothing makes heroku treat your project as a PHP app.
-Nothing to see here, move along. -->
